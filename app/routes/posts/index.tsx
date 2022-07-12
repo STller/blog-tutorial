91b96a5a557bcafaa06f3506ba0af24359d5238c
STller <1165975843@qq.com>
2022-07-12 23:21:58 +0800

    Update index.tsx

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,27 +1,46 @@
-import { json } from '@remix-run/node'
-import { useLoaderData } from '@remix-run/react'
+import { json } from "@remix-run/node";
+import { useLoaderData, Link } from "@remix-run/react";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Array<Post>;
+};
 
 export const loader = () => {
-    return json({
-        posts: [
-            {
-                slug:'my-first-post',
-                title:'My First Post',
-            },
-            {
-                slug:'90s-mixtape',
-                title:'A Mixtape I Made Just For You',
-            },
-        ]
-    })
-}
+  return json<LoaderData>({
+    posts: [
+      {
+        slug: "my-first-post",
+        title: "My First Post",
+      },
+      {
+        slug: "90s-mixtape",
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
 
 export default function Posts() {
-    const { posts } = useLoaderData()
-    console.log(posts)
-    return (
-        <main>
-            <h1>Posts</h1>
-        </main>
-    )
+  const { posts } = useLoaderData() as LoaderData;
+  
+  return (
+    <main>
+      <h1>Posts</h1>
+
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
